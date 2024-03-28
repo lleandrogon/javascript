2977fe7c3deb9e555702f518584ecbf364187e20
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-03-28 16:31:31 -0300

    primeira metade do exercicio

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,8 @@
+function contar() {
+    var ini = document.getElementById('iinicio')
+    var fim = document.getElementById('ifim')
+    var pas = document.getElementById('ipasso')
+    var res = document.getElementById('res')
+    res.innerHTML = 'Contando...'
+    
+}
